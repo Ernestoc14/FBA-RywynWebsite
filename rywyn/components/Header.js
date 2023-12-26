cd8edb600ee1b1491c5c6d20e4ad03a2f9c94789
Ernestoc14 <descreed34@hotmail.com>
2023-12-25 23:55:52 -0500

    Fix: Removing a Margin Top from a li in Navbar

--- rywyn/components/Header.js
+++ rywyn/components/Header.js
@@ -73,7 +73,7 @@ export default function Header() {
               </li>
               <li>
                 <a
-                  className="inline-flex items-center px-4 py-2 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent rounded-lg text-md md:mt-0 md:ml-4 bg-gray-900"
+                  className="inline-flex items-center px-4 py-2 font-medium text-gray-800 transition duration-500 ease-in-out transform bg-transparent rounded-lg text-md md:mt-0 md:ml-4 bg-gray-900"
                   href="/"
                 >
                   <span className="justify-center">Download</span>
