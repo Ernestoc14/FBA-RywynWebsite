89104f16af858c6c21ca2e9b3adcba9303ff7071
Ernestoc14 <descreed34@hotmail.com>
2024-01-09 23:59:08 -0500

    Feat: Refactor pricing page to display product data

--- rywyn/app/pricing/page.jsx
+++ rywyn/app/pricing/page.jsx
@@ -1,22 +1,32 @@
 import { Stripe } from 'stripe'
+import Image from 'next/image'
 
 async function loadPrices() {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
-  const prices = await stripe.prices.list()
-  return prices.data
+  const { data: prices } = await stripe.prices.list({
+    active: true,
+    limit: 10,
+    expand: ['data.product']
+  })
+  return prices
 }
 
 async function PricingPage() {
   const prices = await loadPrices()
 
   return(
-    <div>
-      <h1>Pricing</h1>
+    <div className='grid justify-center items-center md:grid-cols-2 lg:grid-cols-4 gap-x2 bg-white text-black'>
+      <h1 className='my-5 text-center'>Pricing</h1>
       {
         prices.map(price => (
-          <div key={price.id}>
-            <h3>{price.name}</h3>
-            <h2>{price.unit_amount / 100} $</h2>
+          <div key={price.id} className='mb-2 p-6 border-2 border-red-800'>
+            <h3>{price.product.name}</h3>
+            <h2 className='text-center text-2xl font-bold'>{price.unit_amount / 100} $</h2>
+            <div className=''>
+              <img src={price.product.images} alt={price.product.name} 
+                className='w-40 h-40 object-cover' 
+              />
+            </div>
           </div>
         ))
       }
