1a8dae98251365ace7e246fce3a59278937215a9
Ernestoc14 <descreed34@hotmail.com>
2024-01-23 21:47:24 -0500

    Feat: Update function to handle edge cases

---
+++ rywyn/pages/success.js
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <div className="text-black">
+      <Head>
+        <title>Rywyn Resistance Bands</title>
+        <link rel="icon" href="images/rywyn-logo-e.jpeg" />
+      </Head>
+      <Header />
+      <h1>Products</h1>
+      <p>dfkjaskfj
+        sdafkajfa
+      </p>
+      <p>dfksajf</p>
+      <h1>sdj</h1>
+      <h1>Products</h1>
+      <p>dfkjaskfj
+        sdafkajfa
+      </p>
+      <p>dfksajf</p>
+      <h1>sdj</h1>
+      <Footer />
+    </div>
+  );
+}
+
+// This will be the Success Page redirect user After checkout
