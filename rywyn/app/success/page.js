52fdf497570056866d21693b2acd07d3770259cf
Ernestoc14 <descreed34@hotmail.com>
2024-02-06 23:46:20 -0500

    Refactor success page component

--- rywyn/app/success/page.js
+++ rywyn/app/success/page.js
@@ -1,39 +1,38 @@
-// import React from 'react'
+import React from "react";
 
-// function page() {
-//   return (
-//     <div>Success Page</div>
-//   )
-// }
-
-// export default page
-
-import Head from "next/head";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 function page() {
-  return (
-    <div className="text-black">
-      <Head>
-        <title>Rywyn Resistance Bands</title>
-        <link rel="icon" href="public/images/rywyn-logo.jpeg" />
-      </Head>
-      <Header />
-      <h1>Products</h1>
-      <p>dfkjaskfj
-        sdafkajfa
-      </p>
-      <p>dfksajf</p>
-      <h1>sdj</h1>
-      <h1>Products</h1>
-      <p>dfkjaskfj
-        sdafkajfa
-      </p>
-      <p>dfksajf</p>
-      <h1>sdj</h1>
-      <Footer />
-    </div>
-  );
+  return <div>Success Page</div>;
 }
 
-export default page
+export default page;
+
+// import Head from "next/head";
+// import Header from "@/components/Header";
+// import Footer from "@/components/Footer";
+// function page() {
+//   return (
+//     <div className="">
+//       <h1>SUCCESS PAGE</h1>
+//       {/* <Head>
+//         <title>Rywyn Resistance Bands</title>
+//         <link rel="icon" href="public/images/rywyn-logo.jpeg" />
+//       </Head>
+//       <Header />
+//       <h1>Products</h1>
+//       <p>dfkjaskfj
+//         sdafkajfa
+//       </p>
+//       <p>dfksajf</p>
+//       <h1>sdj</h1>
+//       <h1>Products</h1>
+//       <p>dfkjaskfj
+//         sdafkajfa
+//       </p>
+//       <p>dfksajf</p>
+//       <h1>sdj</h1>
+//       <Footer /> */}
+//     </div>
+//   );
+// }
+
+// export default page;
